88fe0fe3266d5065c127c0884fe9bbe658b19571
cheng <chengl@bu.edu>
2024-01-29 02:37:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+            textView.text = getString(R.string.display_text)
+        }
+    }
+}
